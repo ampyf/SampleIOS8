d86cd193096a63f714cf55221e74bd0ce215830a
Ampy <ampy@Ampys-iMac.local>
2016-03-13 11:22:37 -0700

    Initial Commit

---
+++ SampleIOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleIOS8
+//
+//  Created by Ampy on 3/13/16.
+//  Copyright (c) 2016 Ampy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
